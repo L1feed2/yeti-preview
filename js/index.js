c08a39c89cdf6836bac98652aafb1b7bd1d4ced1
L1feed2 <67605178+L1feed2@users.noreply.github.com>
2020-08-12 23:13:52 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -43,3 +43,29 @@ $('ul.desc__tabs-caption').on('click', 'li:not(.active)', function () {
         .closest('div.desc__tabs').find('div.desc__tabs-item').removeClass('desc__tabs-item--active').eq($(this).index()).addClass('desc__tabs-item--active');
 });        
 
+
+// select
+const selectSingle = document.querySelector('.__select');
+const selectSingle_title = selectSingle.querySelector('.__select__title');
+const selectSingle_labels = selectSingle.querySelectorAll('.__select__label');
+
+// Toggle menu
+selectSingle_title.addEventListener('click', () => {
+  if ('active' === selectSingle.getAttribute('data-state')) {
+    selectSingle.setAttribute('data-state', '');
+  } else {
+    selectSingle.setAttribute('data-state', 'active');
+  }
+});
+
+// Close when click to option
+for (let i = 0; i < selectSingle_labels.length; i++) {
+  selectSingle_labels[i].addEventListener('click', (evt) => {
+    selectSingle_title.textContent = evt.target.textContent;
+    selectSingle.setAttribute('data-state', '');
+  });
+}
+
+
+
+
